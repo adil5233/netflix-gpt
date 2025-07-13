2a99f8c6041e10aef56022734f8a8fa67164defd
Adil <adil.amin820@gmail.com>
2025-07-14 00:49:27 +0530

    remove login 2 , add another method for routing, update readme

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,16 +1,39 @@
 import React from "react";
 import Login from "./Login";
 import Browse from "./Browse";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
 const Body = () => {
+  // 1. Method 1
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Login />,
+    },
+    {
+      path: "/browse",
+      element: <Browse />,
+    },
+  ]);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/browse" element={<Browse />} />
-      </Routes>
-    </BrowserRouter>
+    // 2. Method 2
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<Login />} />
+    //     <Route path="/browse" element={<Browse />} />
+    //   </Routes>
+    // </BrowserRouter>
+
+    <div>
+      <RouterProvider router={appRouter} />
+    </div>
   );
 };
 
