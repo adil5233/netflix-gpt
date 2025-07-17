64b37e543ffb1f26f083f3b2b3ecd6f5b3af9ee7
Adil <adil.amin820@gmail.com>
2025-07-17 22:19:11 +0530

    create new custom hook useMovieTrailer

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,26 +1,10 @@
-import React, { useEffect } from "react";
-import { API_OPTIONS } from "../utils/constants";
-import { useDispatch, useSelector } from "react-redux";
-import { addTrailerVideo } from "../utils/moviesSlice";
+import React from "react";
+import { useSelector } from "react-redux";
+import useMovieTrailer from "../hooks/useMovieTrailer";
 
 const VideoBackground = ({ movieId }) => {
-  const dispatch = useDispatch();
   const trailer = useSelector((store) => store.movies?.trailerVideo);
-  const getMoviesVideos = async () => {
-    const data = await fetch(
-      `https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`,
-      API_OPTIONS
-    );
-    const json = await data.json();
-
-    const filterData = json.results.filter((video) => video.type === "Trailer");
-    const trailer = filterData.length ? filterData[0] : json.results[0];
-    dispatch(addTrailerVideo(trailer));
-  };
-
-  useEffect(() => {
-    getMoviesVideos();
-  }, []);
+  useMovieTrailer(movieId);
 
   if (!trailer) return null;
 
@@ -28,7 +12,7 @@ const VideoBackground = ({ movieId }) => {
     <div className="w-full aspect-video relative overflow-hidden">
       <iframe
         className="w-full h-full absolute top-0 left-0 pointer-events-none scale-125"
-        src={`https://www.youtube.com/embed/${trailer.key}?autoplay=1&mute=1&loop=1&playlist=${trailer.key}&controls=0&showinfo=0&modestbranding=1&iv_load_policy=3&fs=0&cc_load_policy=0&start=0&end=0`}
+        src={`https://www.youtube.com/embed/${trailer?.key}?autoplay=1&mute=1&loop=1&playlist=${trailer?.key}&controls=0&showinfo=0&modestbranding=1&iv_load_policy=3&fs=0&cc_load_policy=0&start=0&end=0`}
         title="Netflix Movie Trailer"
         allow="autoplay; encrypted-media"
         allowFullScreen
