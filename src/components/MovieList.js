284cc249dd6233b8e99336dfcba90b24999b98d7
Adil <adil.amin820@gmail.com>
2025-07-18 01:31:11 +0530

    hide scrollbar and some ui changes

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -4,9 +4,9 @@ import MovieCard from "./MovieCard";
 const MovieList = ({ title, movies }) => {
   //   if (!movies || movies.length === 0) return null; // Early return if movies is null/empty
   return (
-    <div className="px-6 bg-black">
+    <div className="px-6">
       <h1 className="text-3xl py-4 text-white">{title}</h1>
-      <div className="flex overflow-x-scroll scrollbar-hide">
+      <div className="flex overflow-x-scroll hide-scrollbar">
         <div className="flex">
           {movies?.map((a) => (
             <MovieCard key={a.id} posterPath={a.poster_path} />
--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -7,14 +7,19 @@ const SecondaryContainer = () => {
   console.log(movies);
   return (
     movies.nowPlayingMovies && (
-      <div>
-        <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
-        <MovieList title={"Trending"} movies={movies.nowPlayingMovies} />
-        <MovieList title={"Popular"} movies={movies.nowPlayingMovies} />
-        <MovieList title={"Upcoming Movies"} movies={movies.nowPlayingMovies} />
-        <MovieList title={"Hollywood"} movies={movies.nowPlayingMovies} />
-        <MovieList title={"Bollywood"} movies={movies.nowPlayingMovies} />
-        <MovieList title={"Horror"} movies={movies.nowPlayingMovies} />
+      <div className="bg-black">
+        <div className="z-20 relative -mt-40">
+          <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
+          <MovieList title={"Trending"} movies={movies.nowPlayingMovies} />
+          <MovieList title={"Popular"} movies={movies.nowPlayingMovies} />
+          <MovieList
+            title={"Upcoming Movies"}
+            movies={movies.nowPlayingMovies}
+          />
+          <MovieList title={"Hollywood"} movies={movies.nowPlayingMovies} />
+          <MovieList title={"Bollywood"} movies={movies.nowPlayingMovies} />
+          <MovieList title={"Horror"} movies={movies.nowPlayingMovies} />
+        </div>
       </div>
     )
   );
