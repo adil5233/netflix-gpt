effdc571c535db0a5a4181b7f15b356813726fa2
Adil <adil.amin820@gmail.com>
2025-07-28 23:03:53 +0530

    [BugFix]: Video Title and Background Video not in sync

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,18 +1,19 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useSelector } from "react-redux";
 import VideoTitle from "./VideoTitle";
 import VideoBackground from "./VideoBackground";
 
 const MainContainer = () => {
-  const movies = useSelector((store) => store.movies?.nowPlayingMovies);
+  const movies = useSelector((store) => store.movies?.nowPlayingMovies ?? []);
 
-  if (!movies) return; // this known as early return
+  // Generate a random index between 0 and 19 and store in useRef
+  const randomIndexRef = useRef(Math.floor(Math.random() * 20));
+  console.log(randomIndexRef);
 
-  // Generate a random index between 0 and 19
-  const randomIndex = Math.floor(Math.random() * 20);
-  const mainMovie = movies[randomIndex];
-  console.log(mainMovie);
+  // If no movies, render nothing
+  if (movies.length === 0) return null;
 
+  const mainMovie = movies[randomIndexRef.current];
   const { original_title, overview, id } = mainMovie;
 
   return (
--- src/hooks/useMovieTrailer.js
+++ src/hooks/useMovieTrailer.js
@@ -5,24 +5,36 @@ import { useEffect } from "react";
 
 const useMovieTrailer = (movieId) => {
   const dispatch = useDispatch();
-
   const trailerVideo = useSelector((store) => store.movies.trailerVideo);
 
-  const getMoviesVideos = async () => {
-    const data = await fetch(
-      `https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`,
-      API_OPTIONS
-    );
-    const json = await data.json();
-
-    const filterData = json.results.filter((video) => video.type === "Trailer");
-    const trailer = filterData.length ? filterData[0] : json.results[0];
-    dispatch(addTrailerVideo(trailer));
-  };
-
   useEffect(() => {
-    !trailerVideo && getMoviesVideos();
-  }, []);
+    // Clear any previous trailer so that loading UI appears on each change
+    dispatch(addTrailerVideo(null));
+
+    // Fetch fresh videos for the new movieId
+    const getMoviesVideos = async () => {
+      try {
+        const data = await fetch(
+          `https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`,
+          API_OPTIONS
+        );
+        const json = await data.json();
+        const filterData = json.results.filter(
+          (video) => video.type === "Trailer"
+        );
+        const trailer = filterData.length ? filterData[0] : json.results[0];
+        dispatch(addTrailerVideo(trailer));
+      } catch (err) {
+        console.error("Failed to fetch trailer:", err);
+      }
+    };
+
+    if (movieId) {
+      getMoviesVideos();
+    }
+  }, [movieId, dispatch]);
+
+  return trailerVideo;
 };
 
 export default useMovieTrailer;
